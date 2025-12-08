9affb53311b385d55b9f3b83ca002b2b7d623f4e
NicoMB2002 <nicomoncayo@hotmail.com>
2025-12-08 12:51:04 -0500

    Fixed dependency Issues and made changes to homepage for better display

--- yum_burger/lib/Views/home.dart
+++ yum_burger/lib/Views/home.dart
@@ -5,7 +5,6 @@ import '../Views/login.dart';
 import '../Views/FAQ.dart';
 import '../Views/AboutUs.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -16,191 +15,181 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Color(0xFFEEE8DE),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Order A YUM\nCombo\nToday!",
-                            style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.brown,
-                            ),
-                          ),
-
-                          const SizedBox(height: 20),
-
-                          ElevatedButton.icon(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => MenuPage()),
-                              );
-                            },
-                            icon: Icon(Icons.shopping_bag),
-                            label: Text("Check the menu"),
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.red.shade800,
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 26, vertical: 14),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          OutlinedButton.icon(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => LoginPage()),
-                              );
-                            },
-                            icon: Icon(Icons.person),
-                            label: Text("Sign up"),
-                            style: OutlinedButton.styleFrom(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 26, vertical: 14),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30),
-                              ),
-                            ),
-                          ),
-                        ],
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    "Order A YUM\nCombo\nToday!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'HoltwoodOneSC',
+                      fontSize: 28,
+                      color: Colors.brown,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Image.asset(
+                    'assets/images/combo.jpg',
+                    width: screenWidth * 0.8,
+                    height: 180,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MenuPage()),
+                      );
+                    },
+                    icon: Icon(Icons.shopping_bag),
+                    label: Text("Check the menu"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red.shade800,
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 26, vertical: 14),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
                       ),
                     ),
-                    Image.asset('assets/images/combo.jpg', width: 400, height: 200,),
-                  ],
-                ),
-                SizedBox(height: 40),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    infoCard(
-                      context: context,
-                      image:
-                      "assets/images/hamburger4.jpg",
-                      title: "Sign up or sign in to\nyour account to make an order!",
-                      buttonText: "Sign Up",
-                      page: LoginPage(),
-                    ),
-                    infoCard(
-                      context: context,
-                      image:
-                      "assets/images/hamburger1.jpg",
-                      title: "Try Out Our Yum\nBacon Burger",
-                      buttonText: "Order Now",
-                      page: MenuPage()
-                    ),
-                  ],
-                ),
-                SizedBox(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    infoCard(
-                      context: context,
-                      image:
-                      "assets/images/hamburger2.jpg",
-                      title: "Want to learn how\nwe make our meals?",
-                      buttonText: "About Us",
-                      page: AboutUsPage()
-                    ),
-                    infoCard(
-                      context: context,
-                      image:
-                      "assets/images/questions.jpg",
-                      title: "Have any questions?",
-                      buttonText: "FAQ",
-                      page: FAQPage()
-                    ),
-                  ],
-                ),
-                SizedBox(height: 50),
-                Row(
-                  children: [
-                    SizedBox(width: 10),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      child: Image.network(
-                        "assets/images/account.jpg",
-                        height: 280,
-                        width: 280,
-                        fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 10),
+                  OutlinedButton.icon(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    },
+                    icon: Icon(Icons.person),
+                    label: Text("Sign up"),
+                    style: OutlinedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 26, vertical: 14),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
                       ),
                     ),
-                    SizedBox(width: 20),
-                    Expanded(
-                      child: Text(
-                        "Create your account and\n"
-                            "check out the latest\n"
-                            "promotions",
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.brown,
-                        ),
-                      ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              // Info Cards Grid
+              GridView.count(
+                crossAxisCount: screenWidth < 600 ? 1 : 2,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                children: [
+                  infoCard(
+                    context: context,
+                    image: "assets/images/hamburger1.jpg",
+                    title: "Check out our latest offers! \n Save up some money for your next meal!",
+                    buttonText: "Check Offers",
+                    page: LoginPage(),
+                  ),
+                  infoCard(
+                    context: context,
+                    image: "assets/images/hamburger4.jpg",
+                    title: "Try Out Our Yum\nBacon Burger",
+                    buttonText: "Order Now",
+                    page: MenuPage(),
+                  ),
+                  infoCard(
+                    context: context,
+                    image: "assets/images/hamburger2.jpg",
+                    title: "Want to learn how\nwe make our meals?",
+                    buttonText: "About Us",
+                    page: AboutUsPage(),
+                  ),
+                  infoCard(
+                    context: context,
+                    image: "assets/images/questions.jpg",
+                    title: "Have any questions?",
+                    buttonText: "FAQ",
+                    page: FAQPage(),
+                  ),
+                ],
+              ),
+              SizedBox(height: 40),
+              // Promo Section
+              Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.asset(
+                      "assets/images/account.jpg",
+                      height: 200,
+                      width: screenWidth * 0.8,
+                      fit: BoxFit.cover,
                     ),
-                  ],
-                ),
-                SizedBox(height: 40),
-              ],
-            ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Create your account and\ncheck out the latest promotions",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.brown,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        )
-      )
+        ),
+      ),
     );
-
-      }
+  }
 }
 
+// Reusable Info Card
 Widget infoCard({
   required BuildContext context,
   required String image,
   required String title,
   required String buttonText,
-  required Widget page
+  required Widget page,
 }) {
   return Container(
-    width: 250,
     decoration: BoxDecoration(
       color: Colors.white,
       borderRadius: BorderRadius.circular(16),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black12,
+          blurRadius: 6,
+          offset: const Offset(0, 3),
+        ),
+      ],
     ),
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-
         ClipRRect(
           borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-          child: Image.network(
+          child: Image.asset(
             image,
-            height: 160,
+            height: 140,
             width: double.infinity,
             fit: BoxFit.cover,
           ),
         ),
-
         Padding(
-          padding: EdgeInsets.all(14),
+          padding: EdgeInsets.all(12),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-
               Text(
                 title,
                 style: TextStyle(
@@ -208,18 +197,14 @@ Widget infoCard({
                   fontWeight: FontWeight.w600,
                 ),
               ),
-
               SizedBox(height: 10),
-
               Align(
                 alignment: Alignment.bottomRight,
                 child: OutlinedButton(
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(
-                        builder: (context) => page,
-                      ),
+                      MaterialPageRoute(builder: (context) => page),
                     );
                   },
                   child: Text(buttonText),
