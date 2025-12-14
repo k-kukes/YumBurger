b5ae81b8017989c5fd475931ab4fecffde999ec1
NicoMB2002 <nicomoncayo@hotmail.com>
2025-12-14 17:40:08 -0500

    Added About us Page with styling

--- yum_burger/lib/Views/AboutUs.dart
+++ yum_burger/lib/Views/AboutUs.dart
@@ -6,60 +6,60 @@ class AboutUsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFEEE8DE),
       appBar: AppBar(
-        title: Text('Beautiful Lake'),
+        centerTitle: true,
+        title: Text(
+          "About Us",
+          style: TextStyle(
+            fontFamily: 'HoltwoodOneSC',
+            fontSize: 28,
+            color: Colors.brown,
+          ),
+        ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image(image: AssetImage('asset/lake-widget-sample.jpg'), width: 300, height: 300,),
-                Image(image: AssetImage('asset/lake-widget-sample2.jpg'), width: 300, height: 300,),
-                Image(image: AssetImage('asset/lake-widget-sample3.jpg'), width: 300, height: 300,),
-              ],
-            ),
-            SizedBox(height: 10,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  children: [
-                    Icon(
-                      Icons.mail_lock,
-                      color: Colors.blue,
-                    ),
-                    SizedBox(height: 7.5,),
-                    Text('Call'),
-                  ],
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Welcome to Yum Burger!",
+                style: TextStyle(
+                  fontFamily: 'HoltwoodOneSC',
+                  fontSize: 28,
+                  color: Colors.brown,
                 ),
-                Column(
-                  children: [
-                    Icon(
-                      Icons.podcasts,
-                      color: Colors.blue,
-                    ),
-                    SizedBox(height: 7.5,),
-                    Text('Share'),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Icon(
-                      Icons.beach_access,
-                      color: Colors.blue,
-                    ),
-                    SizedBox(height: 7.5,),
-                    Text('Route'),
-                  ],
-                ),
-              ],
-            )
-          ],
+              ),
+              SizedBox(height: 16),
+              Text(
+                "At Yum Burger, we believe a great burger is more than just a meal, it’s an experience. "
+                    "Created with fresh ingredients and bold flavors, we serve handcrafted burgers made to satisfy every craving.\n\n"
+                    "From our delicious beef patties to our plant‑based creations, every bite is cooked with care and served with a smile. "
+                    "Whether you’re dining in, ordering online, or grabbing a quick bite on the go, Yum Burger is here to make your day better.\n\n"
+                    "We’re proud to be part of the community, offering friendly service, sustainable practices, and a menu that brings people together. "
+                    "So come hungry, leave happy, and remember: life’s too short for boring burgers!",
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/account.jpg',
+                    width:  400,
+                    height: 400,
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
+
   }
 }
 
